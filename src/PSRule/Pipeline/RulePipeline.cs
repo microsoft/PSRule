6a410904936baa2c37d613d68b405e1373b14d1e
Bernie White <bewhite@microsoft.com>
2019-01-03 18:06:26 +1000

    Add SuppressFinalize for unsealed class

--- src/PSRule/Pipeline/RulePipeline.cs
+++ src/PSRule/Pipeline/RulePipeline.cs
@@ -27,6 +27,7 @@ namespace PSRule.Pipeline
         public void Dispose()
         {
             Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         protected virtual void Dispose(bool disposing)
