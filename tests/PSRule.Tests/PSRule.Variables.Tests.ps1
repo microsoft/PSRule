269c6634394821704f5f7c5673c8bd3071fee656
Bernie White <bewhite@microsoft.com>
2019-04-25 10:39:29 +1000

    Split up tests into smaller files

---
+++ tests/PSRule.Tests/PSRule.Variables.Tests.ps1
@@ -0,0 +1,47 @@
+#
+# Unit tests for PSRule variables
+#
+
+[CmdletBinding()]
+param (
+
+)
+
+# Setup error handling
+$ErrorActionPreference = 'Stop';
+Set-StrictMode -Version latest;
+
+if ($Env:SYSTEM_DEBUG -eq 'true') {
+    $VerbosePreference = 'Continue';
+}
+
+# Setup tests paths
+$rootPath = $PWD;
+
+Import-Module (Join-Path -Path $rootPath -ChildPath out/modules/PSRule) -Force;
+
+$here = (Resolve-Path $PSScriptRoot).Path;
+$outputPath = Join-Path -Path $rootPath -ChildPath out/tests/PSRule.Tests/Variables;
+Remove-Item -Path $outputPath -Force -Recurse -Confirm:$False -ErrorAction Ignore;
+$Null = New-Item -Path $outputPath -ItemType Directory -Force;
+
+#region PSRule variables
+
+Describe 'PSRule variables' -Tag 'Variables' {
+    Context 'PowerShell automatic variables' {
+        $testObject = [PSCustomObject]@{
+            Name = 'VariableTest'
+            Type = 'TestType'
+        }
+        $testObject.PSObject.TypeNames.Insert(0, $testObject.Type);
+
+        It '$Rule' {
+            $result = $testObject | Invoke-PSRule -Path (Join-Path -Path $here -ChildPath 'FromFile.Rule.ps1') -Name 'VariableTest';
+            $result | Should -Not -BeNullOrEmpty;
+            $result.IsSuccess() | Should -Be $True;
+            $result.TargetName | Should -Be 'VariableTest';
+        }
+    }
+}
+
+#endregion PSRule variables
