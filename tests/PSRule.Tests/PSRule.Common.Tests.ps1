2f7e2ce02fbe58d5594475da66875ff9784bd8f0
Bernie White <bewhite@microsoft.com>
2019-06-15 13:58:23 +1000

    Fix options schema and unit test

--- tests/PSRule.Tests/PSRule.Common.Tests.ps1
+++ tests/PSRule.Tests/PSRule.Common.Tests.ps1
@@ -376,7 +376,7 @@ Describe 'Invoke-PSRule' -Tag 'Invoke-PSRule','Common' {
             $resultCsv[1].Synopsis | Should -Be 'Test rule 3';
             $resultCsv[2].RuleName | Should -Be 'WithCsv';
             $resultCsv[2].Synopsis | Should -Be 'This is "a" synopsis.';
-            ($resultCsv[2].Recommendation -replace "`r`n", "`r") | Should -Be "This is an extended recommendation.`r`r- That includes line breaks`r- And lists";
+            ($resultCsv[2].Recommendation -replace "`r`n", "`n") | Should -Be "This is an extended recommendation.`n`n- That includes line breaks`n- And lists";
         }
     }
 
