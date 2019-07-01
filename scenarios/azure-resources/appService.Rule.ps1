686976f2d7d789da54047dcdb229342a5fd6a6fa
Bernie White <bewhite@microsoft.com>
2019-07-02 06:41:26 +1000

    Update documentation

--- scenarios/azure-resources/appService.Rule.ps1
+++ scenarios/azure-resources/appService.Rule.ps1
@@ -4,14 +4,14 @@
 
 # Synopsis: App Service Plan has multiple instances
 Rule 'appServicePlan.MinInstanceCount' -If { ResourceType 'Microsoft.Web/serverfarms' } {
-    Hint 'Use at least two (2) instances'
+    Recommend 'Use at least two (2) instances'
 
     $TargetObject.Sku.capacity -ge 2
 }
 
 # Synopsis: Use at least a Standard App Service Plan
 Rule 'appServicePlan.MinPlan' -If { ResourceType 'Microsoft.Web/serverfarms' } {
-    Hint 'Use a Standard or high plans for production services'
+    Recommend 'Use a Standard or high plans for production services'
 
     ($TargetObject.Sku.tier -eq 'PremiumV2') -or
     ($TargetObject.Sku.tier -eq 'Premium') -or
@@ -19,15 +19,15 @@ Rule 'appServicePlan.MinPlan' -If { ResourceType 'Microsoft.Web/serverfarms' } {
 }
 
 # Synopsis: Disable client affinity for stateless services
-Rule 'appServiceApp.ARRAfinity' -If { ResourceType 'Microsoft.Web/sites' } {
-    Hint 'Disable ARR affinity when not required'
+Rule 'appServiceApp.ARRAffinity' -If { ResourceType 'Microsoft.Web/sites' } {
+    Recommend 'Disable ARR affinity when not required'
 
     $TargetObject.Properties.clientAffinityEnabled -eq $False
 }
 
 # Synopsis: Use HTTPS only
 Rule 'appServiceApp.UseHTTPS' -If { ResourceType 'Microsoft.Web/sites' } {
-    Hint 'Disable HTTP when not required'
+    Recommend 'Disable HTTP when not required'
 
     $TargetObject.Properties.httpsOnly -eq $True
 }
