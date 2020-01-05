d309d1d23d871727fd342ec7de5ee2c2564394e9
Bernie White <bewhite@microsoft.com>
2020-01-05 22:00:46 +1000

    Update documentation

--- scenarios/fruit/fruit-example.ps1
+++ scenarios/fruit/fruit-example.ps1
@@ -1,5 +1,5 @@
-#  Copyright (c) Microsoft Corporation.
-#  Licensed under the MIT License.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 # Define objects
 $items = @();
--- scenarios/fruit/isFruit.Rule.ps1
+++ scenarios/fruit/isFruit.Rule.ps1
@@ -1,5 +1,5 @@
-#  Copyright (c) Microsoft Corporation.
-#  Licensed under the MIT License.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 # Synopsis: An example rule
 Rule 'isFruit' {
