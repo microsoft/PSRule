6379e4627921558321313da93bb9a4f50c66331a
ArmaanMcleod <armaan_mcleod@outlook.com>
2022-01-23 23:47:32 +1100

    Cleanup baseline tests

--- tests/PSRule.Tests/BaselineTests.cs
+++ tests/PSRule.Tests/BaselineTests.cs
@@ -69,9 +69,9 @@ namespace PSRule
             Assert.NotNull(config);
             Assert.Equal(2, config.Count);
             Assert.True(config[0].Type == JTokenType.Object);
-            Assert.Equal("abc", (string)config[0]["value1"]);
+            Assert.Equal("abc", config[0]["value1"]);
             Assert.True(config[1].Type == JTokenType.Object);
-            Assert.Equal("def", (string)config[1]["value2"]);
+            Assert.Equal("def", config[1]["value2"]);
 
             // TestBaseline5
             Assert.Equal("TestBaseline5", baseline[4].Name);
@@ -79,10 +79,12 @@ namespace PSRule
             Assert.True(baseline[4].Obsolete);
         }
 
-        [Fact]
-        public void ReadBaselineInModuleYaml()
+        [Theory]
+        [InlineData(SourceType.Yaml, BaselineYamlFileName)]
+        [InlineData(SourceType.Json, BaselineJsonFileName)]
+        public void ReadBaselineInModule(SourceType type, string path)
         {
-            var baseline = GetBaselines(GetSourceInModule(BaselineYamlFileName, "TestModule", SourceType.Yaml));
+            var baseline = GetBaselines(GetSourceInModule(path, "TestModule", type));
             Assert.NotNull(baseline);
             Assert.Equal(5, baseline.Length);
 
@@ -93,36 +95,12 @@ namespace PSRule
             Assert.False(baseline[0].Obsolete);
         }
 
-        [Fact]
-        public void ReadBaselineInModuleJson()
+        [Theory]
+        [InlineData(BaselineYamlFileName)]
+        [InlineData(BaselineJsonFileName)]
+        public void FilterBaseline(string path)
         {
-            var baseline = GetBaselines(GetSourceInModule(BaselineJsonFileName, "TestModule", SourceType.Json));
-            Assert.NotNull(baseline);
-            Assert.Equal(5, baseline.Length);
-
-            // TestBaseline1
-            Assert.Equal("TestBaseline1", baseline[0].Name);
-            Assert.Equal("github.com/microsoft/PSRule/v1", baseline[0].ApiVersion);
-            Assert.Equal("value", baseline[0].Metadata.Annotations["key"]);
-            Assert.False(baseline[0].Obsolete);
-        }
-
-        [Fact]
-        public void FilterBaselineYaml()
-        {
-            var baseline = GetBaselines(GetSource(BaselineYamlFileName));
-            Assert.NotNull(baseline);
-
-            var filter = new BaselineFilter(new string[] { "TestBaseline5" });
-            var actual = baseline.FirstOrDefault(b => filter.Match(b));
-
-            Assert.Equal("TestBaseline5", actual.Name);
-        }
-
-        [Fact]
-        public void FilterBaselineJson()
-        {
-            var baseline = GetBaselines(GetSource(BaselineJsonFileName));
+            var baseline = GetBaselines(GetSource(path));
             Assert.NotNull(baseline);
 
             var filter = new BaselineFilter(new string[] { "TestBaseline5" });
@@ -163,19 +141,19 @@ namespace PSRule
             var actual = (JArray)JsonConvert.DeserializeObject<dynamic>(json);
 
             // TestBaseline1
-            Assert.Equal("github.com/microsoft/PSRule/v1", (string)actual[0]["apiVersion"]);
-            Assert.Equal("Baseline", (string)actual[0]["kind"]);
-            Assert.Equal("TestBaseline1", (string)actual[0]["metadata"]["name"]);
+            Assert.Equal("github.com/microsoft/PSRule/v1", actual[0]["apiVersion"]);
+            Assert.Equal("Baseline", actual[0]["kind"]);
+            Assert.Equal("TestBaseline1", actual[0]["metadata"]["name"]);
             Assert.NotNull(actual[0]["spec"]);
-            Assert.Equal("kind", (string)actual[0]["spec"]["binding"]["field"]["kind"][0]);
-            Assert.Equal("AlternateName", (string)actual[0]["spec"]["binding"]["field"]["uniqueIdentifer"][0]);
-            Assert.Equal("Id", (string)actual[0]["spec"]["binding"]["field"]["uniqueIdentifer"][1]);
-            Assert.Equal("AlternateName", (string)actual[0]["spec"]["binding"]["targetName"][0]);
-            Assert.Equal("kind", (string)actual[0]["spec"]["binding"]["targetType"][0]);
-            Assert.Equal("WithBaseline", (string)actual[0]["spec"]["rule"]["include"][0]);
-            Assert.Equal("value1", (string)actual[0]["spec"]["configuration"]["key1"]);
-            Assert.Equal("abc", (string)actual[0]["spec"]["configuration"]["key2"][0]["value1"]);
-            Assert.Equal("def", (string)actual[0]["spec"]["configuration"]["key2"][1]["value2"]);
+            Assert.Equal("kind", actual[0]["spec"]["binding"]["field"]["kind"][0]);
+            Assert.Equal("AlternateName", actual[0]["spec"]["binding"]["field"]["uniqueIdentifer"][0]);
+            Assert.Equal("Id", actual[0]["spec"]["binding"]["field"]["uniqueIdentifer"][1]);
+            Assert.Equal("AlternateName", actual[0]["spec"]["binding"]["targetName"][0]);
+            Assert.Equal("kind", actual[0]["spec"]["binding"]["targetType"][0]);
+            Assert.Equal("WithBaseline", actual[0]["spec"]["rule"]["include"][0]);
+            Assert.Equal("value1", actual[0]["spec"]["configuration"]["key1"]);
+            Assert.Equal("abc", actual[0]["spec"]["configuration"]["key2"][0]["value1"]);
+            Assert.Equal("def", actual[0]["spec"]["configuration"]["key2"][1]["value2"]);
         }
 
         #region Helper methods
