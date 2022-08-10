fa975ab59b2f8aec9b87998f6d27e649bc695c55
Bernie White <bewhite@microsoft.com>
2022-08-10 14:37:33 +1000

    Improved documentation of classes #1186

--- src/PSRule/Common/KeyMapDictionary.cs
+++ src/PSRule/Common/KeyMapDictionary.cs
@@ -9,15 +9,27 @@ using System.Linq;
 
 namespace PSRule
 {
+    /// <summary>
+    /// A dictionary of key/ value pairs indexed by a string key that is case-insensitive.
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
     public abstract class KeyMapDictionary<TValue> : DynamicObject, IDictionary<string, TValue>
     {
         private readonly Dictionary<string, TValue> _Map;
 
+        /// <summary>
+        /// Create an empty map.
+        /// </summary>
         internal protected KeyMapDictionary()
         {
             _Map = new Dictionary<string, TValue>(StringComparer.OrdinalIgnoreCase);
         }
 
+        /// <summary>
+        /// Create a map intially populated with values copied from an existing instance.
+        /// </summary>
+        /// <param name="map">An existing instance to copy key/ values from.</param>
+        /// <exception cref="ArgumentNullException">Is raised if the map is null.</exception>
         internal protected KeyMapDictionary(KeyMapDictionary<TValue> map)
         {
             if (map == null)
@@ -26,6 +38,10 @@ namespace PSRule
             _Map = new Dictionary<string, TValue>(map._Map, StringComparer.OrdinalIgnoreCase);
         }
 
+        /// <summary>
+        /// Create a map intially populated with values copied from a dictionary.
+        /// </summary>
+        /// <param name="dictionary">An existing dictionary to copy key/ values from.</param>
         internal protected KeyMapDictionary(IDictionary<string, TValue> dictionary)
         {
             _Map = dictionary == null ?
@@ -33,76 +49,111 @@ namespace PSRule
                 new Dictionary<string, TValue>(dictionary, StringComparer.OrdinalIgnoreCase);
         }
 
+        /// <summary>
+        /// Create a map intially populated with values copied from a hashtable.
+        /// </summary>
+        /// <param name="hashtable">An existing hashtable to copy key/ values from.</param>
         internal protected KeyMapDictionary(Hashtable hashtable)
             : this()
         {
             Load(hashtable);
         }
 
+        /// <inheritdoc/>
         public TValue this[string key]
         {
             get => _Map[key];
             set => _Map[key] = value;
         }
 
+        /// <inheritdoc/>
         public ICollection<string> Keys => _Map.Keys;
 
+        /// <inheritdoc/>
         public ICollection<TValue> Values => _Map.Values;
 
+        /// <inheritdoc/>
         public int Count => _Map.Count;
 
+        /// <inheritdoc/>
         public bool IsReadOnly => false;
 
+        /// <inheritdoc/>
         public void Add(string key, TValue value)
         {
             _Map.Add(key, value);
         }
 
+        /// <inheritdoc/>
         public void Add(KeyValuePair<string, TValue> item)
         {
             Add(item.Key, item.Value);
         }
 
+        /// <summary>
+        /// Clear the map of all keys and values.
+        /// </summary>
         public void Clear()
         {
             _Map.Clear();
         }
 
+        /// <inheritdoc/>
         public bool Contains(KeyValuePair<string, TValue> item)
         {
             return ((IDictionary<string, TValue>)_Map).Contains(item);
         }
 
+        /// <summary>
+        /// Determines if a specified key exists in the map.
+        /// </summary>
+        /// <param name="key">The key map.</param>
+        /// <returns></returns>
         public bool ContainsKey(string key)
         {
             return _Map.ContainsKey(key);
         }
 
+        /// <inheritdoc/>
         public void CopyTo(KeyValuePair<string, TValue>[] array, int arrayIndex)
         {
             ((IDictionary<string, TValue>)_Map).CopyTo(array, arrayIndex);
         }
 
+        /// <inheritdoc/>
         public IEnumerator<KeyValuePair<string, TValue>> GetEnumerator()
         {
             return _Map.GetEnumerator();
         }
 
+        /// <summary>
+        /// Remove the key/ value from the map by key.
+        /// </summary>
+        /// <param name="key">The key of the key/ value to remove.</param>
+        /// <returns>Returns <c>true</c> if the element was found and removed.</returns>
         public bool Remove(string key)
         {
             return _Map.Remove(key);
         }
 
+        /// <inheritdoc/>
         public bool Remove(KeyValuePair<string, TValue> item)
         {
             return ((IDictionary<string, TValue>)_Map).Remove(item);
         }
 
+        /// <summary>
+        /// Try to get the value from the specified key.
+        /// </summary>
+        /// <param name="key">The specific key to find in the map.</param>
+        /// <param name="value">The value of the specific key.</param>
+        /// <returns>Returns <c>true</c> if the key was found and <paramref name="value"/> returned.</returns>
         public bool TryGetValue(string key, out TValue value)
         {
             return _Map.TryGetValue(key, out value);
         }
 
+        /// <inheritdoc/>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
@@ -111,6 +162,7 @@ namespace PSRule
         /// <summary>
         /// Load options from a hashtable.
         /// </summary>
+        /// <exception cref="ArgumentNullException">Is raised if the hashtable is null.</exception>
         protected void Load(Hashtable hashtable)
         {
             if (hashtable == null)
@@ -121,8 +173,10 @@ namespace PSRule
         }
 
         /// <summary>
-        /// Load options from environment variables.
+        /// Load values from environment variables into the option.
+        /// Keys that appear in both will replaced by environment variable values.
         /// </summary>
+        /// <exception cref="ArgumentNullException">Is raised if the environment helper is null.</exception>
         internal void Load(string prefix, EnvironmentHelper env, Func<string, string> format = null)
         {
             if (env == null)
@@ -141,8 +195,10 @@ namespace PSRule
         }
 
         /// <summary>
-        /// Load options from a dictionary.
+        /// Load values from a key/ value dictionary into the option.
+        /// Keys that appear in both will replaced by dictionary values.
         /// </summary>
+        /// <exception cref="ArgumentNullException">Is raised if the dictionary is null.</exception>
         protected void Load(string prefix, IDictionary<string, object> dictionary)
         {
             if (dictionary == null)
@@ -176,6 +232,13 @@ namespace PSRule
             return false;
         }
 
+        /// <summary>
+        /// Get the value of a dynamic object member.
+        /// </summary>
+        /// <param name="binder">A dynamic binder object.</param>
+        /// <param name="result">The value of the member.</param>
+        /// <returns>Returns <c>true</c> if the member was found and <c>false</c> if the member was not.</returns>
+        /// <exception cref="ArgumentNullException">Is raised if the binder is null.</exception>
         public sealed override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             if (binder == null)
--- src/PSRule/Pipeline/Source.cs
+++ src/PSRule/Pipeline/Source.cs
@@ -12,17 +12,29 @@ using YamlDotNet.Serialization;
 
 namespace PSRule.Pipeline
 {
+    /// <summary>
+    /// The type of source file.
+    /// </summary>
     public enum SourceType
     {
+        /// <summary>
+        /// PowerShell script file.
+        /// </summary>
         Script = 1,
 
+        /// <summary>
+        /// YAML file.
+        /// </summary>
         Yaml = 2,
 
+        /// <summary>
+        /// JSON or JSON with comments file.
+        /// </summary>
         Json = 3
     }
 
     /// <summary>
-    /// A source file for rule definitions.
+    /// A source file containing resources that will be loaded and interpreted by PSRule.
     /// </summary>
     [DebuggerDisplay("{Type}: {Path}")]
     public sealed class SourceFile
@@ -31,6 +43,13 @@ namespace PSRule.Pipeline
 
         internal Source Source;
 
+        /// <summary>
+        /// Create an instance of a PSRule source.
+        /// </summary>
+        /// <param name="path">The file path to the source.</param>
+        /// <param name="module">The name of the module if the source was loaded from a module.</param>
+        /// <param name="type">The type of source file.</param>
+        /// <param name="helpPath">The base path to use for loading help content.</param>
         public SourceFile(string path, string module, SourceType type, string helpPath)
         {
             Path = path;
@@ -39,20 +58,35 @@ namespace PSRule.Pipeline
             HelpPath = helpPath;
         }
 
+        /// <summary>
+        /// The file path to the source.
+        /// </summary>
         [JsonProperty(PropertyName = "path")]
         public string Path { get; }
 
+        /// <summary>
+        /// The name of the module if the source was loaded from a module.
+        /// </summary>
         [JsonProperty(PropertyName = "moduleName")]
         public string Module { get; }
 
+        /// <summary>
+        /// The name of the module if the source was loaded from a module.
+        /// </summary>
         [JsonIgnore]
         [Obsolete("Use Module property instead.")]
         public string ModuleName => Module;
 
+        /// <summary>
+        /// The type of source file.
+        /// </summary>
         [YamlIgnore]
         [JsonIgnore]
         public SourceType Type { get; }
 
+        /// <summary>
+        /// The base path to use for loading help content.
+        /// </summary>
         [YamlIgnore]
         [JsonIgnore]
         public string HelpPath { get; }
@@ -71,6 +105,9 @@ namespace PSRule.Pipeline
         }
     }
 
+    /// <summary>
+    /// A PSRule source containing one or more source files.
+    /// </summary>
     public sealed class Source
     {
         internal bool Dependency;
@@ -145,8 +182,14 @@ namespace PSRule.Pipeline
             }
         }
 
+        /// <summary>
+        /// The base path of the source.
+        /// </summary>
         public string Path { get; }
 
+        /// <summary>
+        /// An array of source files.
+        /// </summary>
         public SourceFile[] File { get; }
 
         internal string Scope
