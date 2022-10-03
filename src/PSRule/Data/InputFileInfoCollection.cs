c9eb0c712f2db17cad94fb7968117ce6456d847e
Bernie White <bewhite@microsoft.com>
2022-10-03 20:28:16 +1000

    Added comments

--- src/PSRule/Data/InputFileInfoCollection.cs
+++ src/PSRule/Data/InputFileInfoCollection.cs
@@ -8,11 +8,22 @@ using System.Linq;
 
 namespace PSRule.Data
 {
+    /// <summary>
+    /// A collection of <see cref="InputFileInfo"/>.
+    /// </summary>
     public interface IInputFileInfoCollection : IEnumerable<InputFileInfo>
     {
+        /// <summary>
+        /// Filters the collection to only include <see cref="InputFileInfo"/> with a specific file extension.
+        /// </summary>
+        /// <param name="extension">A file extension to filter the collection to.</param>
+        /// <returns>A filtered collection.</returns>
         IInputFileInfoCollection WithExtension(string extension);
     }
 
+    /// <summary>
+    /// A collection of <see cref="InputFileInfo"/>.
+    /// </summary>
     internal sealed class InputFileInfoCollection : IInputFileInfoCollection, IEnumerable<InputFileInfo>
     {
         private readonly IEnumerable<InputFileInfo> _Items;
@@ -31,11 +42,6 @@ namespace PSRule.Data
         {
             return new InputFileInfoCollection(_Items.Where(i => i.Extension == extension));
         }
-
-        //public IInputFileInfoCollection WithPath()
-        //{
-        //    return new InputFileInfoCollection(_Items.Where(i => i.));
-        //}
 
         #region IEnumerable<InputFileInfo>
 
