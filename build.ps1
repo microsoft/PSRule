da7aa30f98f2ab08192d37b58b0590665f57180f
Bernie White <bewhite@microsoft.com>
2023-02-25 22:46:45 +1000

    Updates to build pipeline (#1454)

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@
 # Note:
 # This manually builds the project locally
 
-. ./.azure-pipelines/pipeline-deps.ps1
+. ./scripts/pipeline-deps.ps1
 Invoke-Build Test -AssertStyle Client
 
 Write-Host "If no build errors occured. The module has been saved to out/modules/PSRule"
