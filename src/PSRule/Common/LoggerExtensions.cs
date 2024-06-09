35c9bd821456a30d35413f13bbec9a743797473e
Bernie White <bewhite@microsoft.com>
2024-06-09 22:52:30 +1000

    Refactor to use ILogger #1837

--- src/PSRule/Common/LoggerExtensions.cs
+++ src/PSRule/Common/LoggerExtensions.cs
@@ -1,33 +1,50 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.Extensions.Logging;
 using PSRule.Definitions;
 using PSRule.Pipeline;
 using PSRule.Resources;
-using PSRule.Runtime;
 
 namespace PSRule;
 
+/// <summary>
+/// Extension for <see cref="ILogger"/> to log common messages.
+/// </summary>
 internal static class LoggerExtensions
 {
+    private static readonly EventId PSR0004 = new(4, "PSR0004");
+
     internal static void WarnResourceObsolete(this ILogger logger, ResourceKind kind, string id)
     {
-        if (logger == null || !logger.ShouldLog(LogLevel.Warning))
+        if (logger == null || !logger.IsEnabled(LogLevel.Warning))
             return;
 
-        logger.Warning(PSRuleResources.ResourceObsolete, Enum.GetName(typeof(ResourceKind), kind), id);
+        logger.LogWarning
+        (
+            PSRuleResources.ResourceObsolete,
+            Enum.GetName(typeof(ResourceKind), kind),
+            id
+        );
     }
 
     internal static void ErrorResourceUnresolved(this ILogger logger, ResourceKind kind, string id)
     {
-        if (logger == null || !logger.ShouldLog(LogLevel.Error))
+        if (logger == null || !logger.IsEnabled(LogLevel.Error))
             return;
 
-        logger.Error(new PipelineBuilderException(string.Format(
-            Thread.CurrentThread.CurrentCulture,
+        logger.LogError
+        (
+            PSR0004,
+            new PipelineBuilderException(string.Format(
+                Thread.CurrentThread.CurrentCulture,
+                PSRuleResources.PSR0004,
+                Enum.GetName(typeof(ResourceKind),
+                kind), id
+            )),
             PSRuleResources.PSR0004,
-            Enum.GetName(typeof(ResourceKind),
-            kind), id
-        )), "PSR0004");
+            Enum.GetName(typeof(ResourceKind), kind),
+            id
+        );
     }
 }
