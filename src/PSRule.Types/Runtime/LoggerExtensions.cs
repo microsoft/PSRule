d775e02e03a4e6a29bae0cfe9af77e2f8a7cef7d
Bernie White <bewhite@microsoft.com>
2024-07-13 16:38:41 +1000

    Fixes

--- src/PSRule.Types/Runtime/LoggerExtensions.cs
+++ src/PSRule.Types/Runtime/LoggerExtensions.cs
@@ -4,60 +4,57 @@
 namespace PSRule.Runtime;
 
 /// <summary>
-/// 
+/// Extension for <see cref="ILogger"/> to log diagnostic messages.
 /// </summary>
 public static class LoggerExtensions
 {
     private static readonly Func<FormattedLogValues, Exception?, string> _messageFormatter = MessageFormatter;
 
     /// <summary>
-    /// 
+    /// Log a warning message.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="eventId"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
+    /// <param name="logger">A valid <see cref="ILogger"/> instance.</param>
+    /// <param name="eventId">An event identifier for the warning.</param>
+    /// <param name="message">The format message text.</param>
+    /// <param name="args">Additional arguments to use within the format message.</param>
     public static void LogWarning(this ILogger logger, EventId eventId, string? message, params object?[] args)
     {
-       if (logger == null || !logger.IsEnabled(LogLevel.Warning))
-            return;
-
         logger.Log(LogLevel.Warning, eventId, default, message, args);
     }
 
     /// <summary>
-    /// 
+    /// Log an error message.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="eventId"></param>
-    /// <param name="exception"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
+    /// <param name="logger">A valid <see cref="ILogger"/> instance.</param>
+    /// <param name="eventId">An event identifier for the error.</param>
+    /// <param name="exception">An optional exception which the error message is related to.</param>
+    /// <param name="message">The format message text.</param>
+    /// <param name="args">Additional arguments to use within the format message.</param>
     public static void LogError(this ILogger logger, EventId eventId, Exception? exception, string? message, params object?[] args)
     {
-        if (logger == null || !logger.IsEnabled(LogLevel.Error))
-            return;
-
         logger.Log(LogLevel.Error, eventId, exception, message, args);
     }
 
     /// <summary>
-    /// 
+    /// Log a diagnostic message.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="logLevel"></param>
-    /// <param name="eventId"></param>
-    /// <param name="exception"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
-    /// <exception cref="NullReferenceException"></exception>
+    /// <param name="logger">A valid <see cref="ILogger"/> instance.</param>
+    /// <param name="logLevel">The type of diagnostic message.</param>
+    /// <param name="eventId">An event identifier for the diagnostic message.</param>
+    /// <param name="exception">An optional exception which the diagnostic message is related to.</param>
+    /// <param name="message">The format message text.</param>
+    /// <param name="args">Additional arguments to use within the format message.</param>
     public static void Log(this ILogger logger, LogLevel logLevel, EventId eventId, Exception? exception, string? message, params object?[] args)
     {
-        if (logger == null) throw new NullReferenceException(nameof(logger));
+        if (logger == null || !logger.IsEnabled(logLevel))
+            return;
 
         logger.Log(logLevel, eventId, new FormattedLogValues(message, args), exception, _messageFormatter);
     }
 
+    /// <summary>
+    /// Format log messages with values.
+    /// </summary>
     private static string MessageFormatter(FormattedLogValues state, Exception? error)
     {
         return state.ToString();
