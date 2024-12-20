8609c8f2c59e9668ee067351e68951e6d6c29ea4
Bernie White <bewhite@microsoft.com>
2024-12-20 10:35:24 +1000

    Refactoring for tests

---
+++ tests/PSRule.Tests/TestObject.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace PSRule.Pipeline;
+
+internal sealed class TestObject
+{
+    public string Name { get; set; }
+}
